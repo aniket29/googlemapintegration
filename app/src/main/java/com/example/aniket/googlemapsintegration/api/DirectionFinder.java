75fa37d8747e5fb55484173ff6b556efe33e6709
aniket29 <aniket@hellobuddy.in>
2016-07-03 01:10:38 +0530

    checking in individual items if json returns null or not

--- app/src/main/java/com/example/aniket/googlemapsintegration/api/DirectionFinder.java
+++ app/src/main/java/com/example/aniket/googlemapsintegration/api/DirectionFinder.java
@@ -192,32 +192,44 @@ return responseString;
     private void parseJSon(String data) throws JSONException {
         if (data == null)
             return;
-
+        JSONArray jsonRoutes=null;
         List<Route> routes = new ArrayList<Route>();
         JSONObject jsonData = new JSONObject(data);
-        JSONArray jsonRoutes = jsonData.getJSONArray("routes");
+        if(jsonData.getJSONArray("routes")!=null)
+        {
+        jsonRoutes = jsonData.getJSONArray("routes");
+            if(jsonRoutes!=null){
         for (int i = 0; i < jsonRoutes.length(); i++) {
-            JSONObject jsonRoute = jsonRoutes.getJSONObject(i);
+            JSONObject jsonRoute=null,overview_polylineJson=null,jsonLeg=null, jsonDistance=null,jsonDuration=null,jsonEndLocation=null,jsonStartLocation=null;
+            JSONArray jsonLegs=null;
+            if(jsonRoutes.getJSONObject(i)!=null){
+            jsonRoute= jsonRoutes.getJSONObject(i);
             Route route = new Route();
 
-            JSONObject overview_polylineJson = jsonRoute.getJSONObject("overview_polyline");
-            JSONArray jsonLegs = jsonRoute.getJSONArray("legs");
-            JSONObject jsonLeg = jsonLegs.getJSONObject(0);
-            JSONObject jsonDistance = jsonLeg.getJSONObject("distance");
-            JSONObject jsonDuration = jsonLeg.getJSONObject("duration");
-            JSONObject jsonEndLocation = jsonLeg.getJSONObject("end_location");
-            JSONObject jsonStartLocation = jsonLeg.getJSONObject("start_location");
-
+             overview_polylineJson = jsonRoute.getJSONObject("overview_polyline");
+             jsonLegs = jsonRoute.getJSONArray("legs");
+             jsonLeg = jsonLegs.getJSONObject(0);
+             jsonDistance = jsonLeg.getJSONObject("distance");
+             jsonDuration = jsonLeg.getJSONObject("duration");
+            jsonEndLocation = jsonLeg.getJSONObject("end_location");
+             jsonStartLocation = jsonLeg.getJSONObject("start_location");
+                if(jsonDistance!=null&&jsonDistance.has("text")&&jsonDistance.has("value"))
             route.distance = new Distance(jsonDistance.getString("text"), jsonDistance.getInt("value"));
+                if(jsonDuration!=null&&jsonDuration.has("text")&&jsonDuration.has("value"))
             route.duration = new com.example.aniket.googlemapsintegration.model.Duration(jsonDuration.getString("text"), jsonDuration.getInt("value"));
-            route.endAddress = jsonLeg.getString("end_address");
+                if(jsonLeg!=null&&jsonLeg.has("end_address"))
+                route.endAddress = jsonLeg.getString("end_address");
+                if(jsonLeg!=null&&jsonLeg.has("start_address"))
             route.startAddress = jsonLeg.getString("start_address");
-            route.startLocation = new LatLng(jsonStartLocation.getDouble("lat"), jsonStartLocation.getDouble("lng"));
+                if(jsonStartLocation!=null&&jsonStartLocation.has("lat")&&jsonStartLocation.has("lng"))
+                route.startLocation = new LatLng(jsonStartLocation.getDouble("lat"), jsonStartLocation.getDouble("lng"));
+                if(jsonEndLocation!=null&&jsonEndLocation.has("lat")&&jsonEndLocation.has("lng"))
             route.endLocation = new LatLng(jsonEndLocation.getDouble("lat"), jsonEndLocation.getDouble("lng"));
+                if(overview_polylineJson!=null&&overview_polylineJson.has("points"))
             route.points = decodePolyLine(overview_polylineJson.getString("points"));
 
-            routes.add(route);
-        }
+            routes.add(route);}
+        }}}
 
         listener.onDirectionFinderSuccess(routes);
         }
